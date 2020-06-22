a1870b7f17a40c43e6439a01bf6f005b0884cf6f
mattcbodily <mattcbodily@gmail.com>
2020-06-22 12:39:27 -0600

    finished lecture code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +11,42 @@ class App extends Component {
     }
   }
 
+  componentDidMount(){
+    this.getPokemon();
+  }
+
+  getPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.pokeId}`)
+    .then(res => this.setState({pokemon: res.data}))
+    .catch(err => console.log(err))
+  }
+
+  handleIncrement = () => {
+    this.setState({pokeId: this.state.pokeId + 1});
+    this.getPokemon();
+  }
+
+  handleDecrement = () => {
+    if(this.state.pokeId !== 1){
+      this.setState({pokeId: this.state.pokeId - 1});
+      this.getPokemon();
+    }
+  }
+
   render(){
     return (
       <div className='App'>
         <img className='pokemon-logo' src='https://1000logos.net/wp-content/uploads/2017/05/Pokemon-Logo.png' alt='Pokemon'/>
+        {this.state.pokemon.name
+        ? (
+         <>
+          <p className='poke-name'>{this.state.pokemon.name}</p>
+          <img className='poke-image' src={this.state.pokemon.sprites.front_default} alt={this.state.pokemon.name}/>
+         </> 
+        )
+        : null}
+        <button onClick={this.handleDecrement}>Previous</button>
+        <button onClick={this.handleIncrement}>Next</button>
       </div>
     )
   }
